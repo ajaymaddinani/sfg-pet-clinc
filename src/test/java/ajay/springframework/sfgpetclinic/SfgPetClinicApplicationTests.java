b49e3083f45ab8572a6ea2e590a4a39fc7257c63
Ajay Babu Maddinani <you@example.com>
2021-07-23 22:35:36 +0800

    Intial Commit

---
+++ src/test/java/ajay/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ajay.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
