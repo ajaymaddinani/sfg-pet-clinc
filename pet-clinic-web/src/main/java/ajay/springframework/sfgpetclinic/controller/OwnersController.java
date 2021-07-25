8c2f2e789b84ebf20b44294690d894ff67f3d6b1
Ajay Babu Maddinani <you@example.com>
2021-07-25 12:54:15 +0800

    Add owners,vets index and controllers

---
+++ pet-clinic-web/src/main/java/ajay/springframework/sfgpetclinic/controller/OwnersController.java
@@ -0,0 +1,16 @@
+package ajay.springframework.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnersController {
+	
+	@RequestMapping({"/owners","/owners/index","/owners/index.html"})
+	public String listOwners()
+	{
+		return "owners/index";
+		
+	}
+
+}		
