9045b3b5b083b43c9383073b5d4affdc750d03a5
Ajay Babu Maddinani <you@example.com>
2021-07-26 14:52:54 +0800

    Add Specality

---
+++ pet-clinic-data/src/main/java/ajay/springframework/sfgpetclinic/model/Specality.java
@@ -0,0 +1,15 @@
+package ajay.springframework.sfgpetclinic.model;
+
+public class Specality extends BaseEntity{
+	
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
--- pet-clinic-data/src/main/java/ajay/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ajay/springframework/sfgpetclinic/model/Vet.java
@@ -1,6 +1,18 @@
 package ajay.springframework.sfgpetclinic.model;
 
+import java.util.Set;
 
 public class Vet extends Person {
+	
+	private Set<Specality> specalities;
 
+	public Set<Specality> getSpecalities() {
+		return specalities;
+	}
+
+	public void setSpecalities(Set<Specality> specalities) {
+		this.specalities = specalities;
+	}
+
+ 
 }
