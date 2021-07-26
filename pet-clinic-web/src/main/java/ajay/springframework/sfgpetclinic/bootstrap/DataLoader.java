1398578634208317d1359602fa2bf3bb52a70181
Ajay Babu Maddinani <you@example.com>
2021-07-26 15:08:50 +0800

    Bootstrap data for PetType

--- pet-clinic-web/src/main/java/ajay/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ajay/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import ajay.springframework.sfgpetclinic.model.Owner;
+import ajay.springframework.sfgpetclinic.model.PetType;
 import ajay.springframework.sfgpetclinic.model.Vet;
 import ajay.springframework.sfgpetclinic.services.OwnerService;
+import ajay.springframework.sfgpetclinic.services.PetTypeSevice;
 import ajay.springframework.sfgpetclinic.services.VetService;
 
 @Component
@@ -13,11 +15,13 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeSevice petTypeService;
 
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeSevice petTypeService) {
 		super();
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	
@@ -44,6 +48,19 @@ public class DataLoader implements CommandLineRunner {
 		vet.setLastName("Maddinani");
 		vetService.save(vet);
 		System.out.println("loaded vets");
+		
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("Cat");
+		petTypeService.save(cat);
+		
+		PetType bird = new PetType();
+		bird.setName("Bird");
+		petTypeService.save(bird);
+		
 	}
 
 }
